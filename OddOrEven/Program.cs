a1413706946466d4330bd7e3b3cfbe6e389f8549
stenliit <sten.liit@tktk.ee>
2024-09-30 11:26:07 +0300

    Add Week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a number");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int result = userNum % 2;
+
+
+
+if (result != 0)
+{
+    Console.WriteLine("User nr is odd");
+}
+else
+{
+    Console.WriteLine("user nr is even");
+}
---
+++ Week3ifAndSwitch/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = 24;
+int b = 8;
+
+int c = b % a;
+
+Console.WriteLine(c);
